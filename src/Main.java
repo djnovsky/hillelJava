3898acd5e2099bbc4d94b6605b8b39439c710aec
Stas Novsky <djnovsky@gmail.com>
2016-02-12 19:39:32 +0200

    123

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 09.02.2016.
+ */
+public class Main {
+    public static void main (String args[]){
+        float people = 30f;
+        float rooms = 0;
+
+
+       if (rooms > 0) {
+           System.out.println(people/rooms);
+       } else {
+           System.out.println("nothing");
+       }
+       }
+    }
+
---
+++ src/Minimum.java
@@ -0,0 +1,26 @@
+/**
+ * Created by User on 12.02.2016.
+ */
+public class Minimum {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int num3 = 2;
+
+        if (num1 > num2) {
+            if (num2 > num3) {
+                System.out.println("Minimum is num3: " + num3);
+            } else {
+                System.out.println("Minimum is num2: " + num2);
+            }
+
+        } else {
+                if (num1 > num3) {
+                    System.out.println("Minimum is num3: " + num3);
+                } else {
+                    System.out.println("Minimum is num1: " + num1);
+                }
+            }
+        }
+    }
+
