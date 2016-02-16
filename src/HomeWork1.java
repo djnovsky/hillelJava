ee5022dcd68c81969f3a577d1109341f488dfa64
Stas Novsky <djnovsky@gmail.com>
2016-02-16 18:59:11 +0200

    123

---
+++ src/HomeWork1.java
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 16.02.2016.
+ */
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int digit = 213;
+        if (digit % 2 ==0)
+            System.out.println("Digit ia even");
+        else
+            System.out.println("Digit is uneven");
+    }
+}
