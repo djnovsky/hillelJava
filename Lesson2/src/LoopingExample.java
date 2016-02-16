f6aee43ce50e1c3851cdb54ebbe79317cf2ae8aa
Stas Novsky <djnovsky@gmail.com>
2016-02-16 20:10:40 +0200

    123

---
+++ Lesson2/src/LoopingExample.java
@@ -0,0 +1,24 @@
+/**
+ * Created by User on 16.02.2016.
+ */
+public class LoopingExample {
+    public static void main(String[] args) {
+        int i =1;
+        /*while (i<=10){
+            System.out.println(i);
+            i++; //++i;
+
+
+        }*/
+        //while (i<=10) {
+      //      System.out.println(i++);
+      //  }
+
+        i=11;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<=10);
+
+    }
+}
