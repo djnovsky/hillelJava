cb7dee76ae82cb32bd0911faacc078ca85b4b8be
Stas Novsky <djnovsky@gmail.com>
2016-02-16 20:10:14 +0200

    123

---
+++ Lesson2/src/LoopingTask.java
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 16.02.2016.
+ */
+public class LoopingTask {
+    public static void main(String[] args) {
+        int factorial = 1;
+        int i = 5;
+
+        while (i >0) {
+            factorial= factorial*i;
+            i--;
+
+
+        }
+        System.out.println("Result is: " + factorial);
+    }
+}
