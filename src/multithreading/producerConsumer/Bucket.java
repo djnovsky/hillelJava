246706b1150ee4e65a599a187a52dc30d4553a33
Stas Novsky <djnovsky@gmail.com>
2016-05-07 12:16:56 +0300

    Class

--- src/multithreading/producerConsumer/Bucket.java
+++ src/multithreading/producerConsumer/Bucket.java
@@ -4,49 +4,51 @@ package multithreading.producerConsumer;
  * Created by User on 06.05.2016.
  */
 public class Bucket {
-
-
     private int counter;
     private long changesCounter;
 
 
-    public synchronized void get() {
-        while (counter <= 0) {
-            try {
-                wait();
-            } catch (InterruptedException ignored) {
-                Thread.currentThread().interrupt();
-                return;
+    public void get() {
+        synchronized (this) {
+            while (counter <= 0) {
+                try {
+                    wait();
+                } catch (InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
             }
-        }
-        counter--;
-        changesCounter++;
-        notifyAll();
-        check();
-
-    }
-
-
-    public synchronized void put() {
-        while (counter < 1000) {
-            try {
-                wait();
-            } catch (InterruptedException ignored) {
-                Thread.currentThread().interrupt();
-                return;
-            }
-            counter++;
+            counter--;
             changesCounter++;
             notifyAll();
             check();
         }
     }
 
-    private void check() {
+
+    public synchronized void put() {
+        while (counter >= 1000) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+        counter++;
+        changesCounter++;
+        notifyAll();
+        check();
+    }
+
+
+    public synchronized void check() {
         if (counter < 0 || counter > 1000) {
             System.out.println("error: counter = " + counter);
         }
     }
+
+
 
     @Override
     public String toString() {
--- src/multithreading/producerConsumer/Main.java
+++ src/multithreading/producerConsumer/Main.java
@@ -4,36 +4,45 @@ package multithreading.producerConsumer;
  * Created by User on 06.05.2016.
  */
 public class Main {
-
     public static void main(String[] args) {
         Bucket bucket = new Bucket();
         Thread consumerThread = new Thread(new Consumer(bucket));
         Thread producerThread = new Thread(new Producer(bucket));
 
+
         consumerThread.start();
         producerThread.start();
 
-        try {
-            consumerThread.join();
-            producerThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         try {
             Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException ignored) {
         }
 
-        interuptAll(consumerThread, producerThread);
 
-        System.out.println("All threads are stopped");
-        System.out.println(bucket.toString());
-    }
-
-    private static void interuptAll(Thread consumerThread, Thread producerThread) {
         consumerThread.interrupt();
         producerThread.interrupt();
+
+
+        try {
+            consumerThread.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+
+
+        try {
+            producerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+
+
+        System.out.println("all threads are stopped");
+        System.out.println(bucket);
+
     }
 }
